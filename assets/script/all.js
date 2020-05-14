6dc36c2922f76125bd094e41239fe570393ccc5a
rockalen <rockalen0523@gmail.com>
2020-05-14 16:37:41 +0800

    Initial commit

---
+++ assets/script/all.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    // 漢堡選單
+    // $('.ham-icon').click(function(e) {
+    //     e.preventDefault();
+    //     $('.navbar').toggleClass('active')
+    // })
+    
+    $('#sub-menu-dropdown').hide();
+    $('#sub-menu-add-admin').hide();
+    
+    let params = (new URL(document.location)).searchParams;
+    // let title = params.get('title'); // is the string "Jonathan Smith".
+    // let age = parseInt(params.get('age')); // is the number 18
+    var title = $(this).attr('title');
+    console.log(title);
+
+    if (title == 'Assignment'){
+        $('#sub-menu-dropdown').show();
+    }else if (title == 'Admin'){
+        $('#sub-menu-add-admin').show();
+    }
+
+
+    $('.custom-btn-success').click(function (e) {
+        // 取消預設 event 事件
+        e.preventDefault();
+        // 顯示編輯視窗
+        $('.replay').show();
+    })
+    
+    $('.custom-btn-cancel').click(function (e) {
+        // 取消預設 event 事件
+        e.preventDefault(); 
+        // 隱藏編輯視窗
+        $('.replay').hide();    
+    })
+});
---
+++ temp.md
@@ -0,0 +1,50 @@
+# 🏅第五週版型攻略
+## 洧杰老師主線攻略教學
+
+同學你好，為了讓您更容易攻破此關卡，老師提供攻略資源給您參考
+
+## 助教直播範例
+[Codepen 實作：使用 Bootstrap 元件](https://codepen.io/AliceChiang/pen/QWjxyxw?editors=1100)
+
+## ICON 載入教學
+
+* 請觀看卡斯伯老師的影片介紹，了解這版型的 ICON 要如何透過 CSS 載入，時間點為 [38 分 00 秒](https://courses.hexschool.com/courses/808825/lectures/16814984)
+
+ 
+## 首頁
+
+左側欄
+- insert_chart
+- forum
+- assignment
+- video_library
+- supervisor_account
+- settings
+
+上方區塊
+- append_more
+- search
+- notifications_active
+
+主內容
+- more_vert
+- star_border
+- edit
+- chevron_left
+- chevron_right
+
+## Admin頁面
+
+上方區塊
+- add
+
+主內容
+- arrow_downward
+- remove_red_eye
+
+## Edit Admin Modal
+- clear
+- check_circle
+
+## New Admin Modal
+- warning
